f0402835e1f1ae428c8593d3a0fe4eeea39128cb
dhananjayjaiswal16 <dhananjayjaiswal16.10@gmail.com>
2022-07-08 12:02:19 +0530

    External Api call

--- src/main/java/com/springrest/courses/service/ProductServiceImpl.java
+++ src/main/java/com/springrest/courses/service/ProductServiceImpl.java
@@ -1,21 +1,52 @@
 package com.springrest.courses.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springrest.courses.entity.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.swing.tree.ExpandVetoException;
+import java.util.ArrayList;
 import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService{
     private Logger log = LoggerFactory.getLogger(this.getClass());
     @Override
     public List<Product> getProducts() {
-        String url = "https://dummyjson.com/products/";
+        String uri = "https://dummyjson.com/products/";
         RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.getForObject(url, String.class);
-        log.info("Response from dummy data : {}",response);
+        ResponseEntity<String> responseEntity = null;
+        List<Product> list;
+        try {
+            responseEntity = restTemplate.getForEntity(uri, String.class);
+        } catch (Exception serviceUnavailableException) {
+            log.error("Error while fetching data : {}", serviceUnavailableException);
+        }
+
+        if (responseEntity != null) {
+            String response = responseEntity.getBody();
+            log.info("Response from dummy api :{}", response);
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                JsonNode jsonNode = objectMapper.readTree(response);
+                JsonNode products = jsonNode.get("products");
+                list = new ArrayList<>();
+                products.forEach(product -> {
+                    list.add(new Product(Long.parseLong(product.get("id").asText()), product.get("title").asText(),
+                            product.get("description").asText(), product.get("price").asInt(), product.get("category").asText()));
+                });
+                return list;
+
+            } catch (Exception e){
+                log.error("Exception is : {}", e);
+            }
+        }
         return null;
     }
 }
